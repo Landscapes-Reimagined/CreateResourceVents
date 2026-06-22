419792996876c8a369243670652a97708599243f
Gamma_02 <gammagames06@gmail.com>
2026-06-22 17:28:28 -0500

    Make sure that this builds with newer versions of copycats+

--- src/main/java/com/landscapesreimagined/createresourcevents/mixin/ChunkParserMixin.java
+++ src/main/java/com/landscapesreimagined/createresourcevents/mixin/ChunkParserMixin.java
@@ -9,11 +9,11 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.chunk.PalettedContainer;
 import net.minecraft.world.level.chunk.storage.ChunkSerializer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-import java.util.List;
 import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
@@ -25,11 +25,11 @@ import java.util.stream.Collector;
 public class ChunkParserMixin {
 
    @WrapOperation(method = "read", at = @At(value = "INVOKE", target = "Lcom/mojang/serialization/Codec;parse(Lcom/mojang/serialization/DynamicOps;Ljava/lang/Object;)Lcom/mojang/serialization/DataResult;", ordinal = 0))
-    private static DataResult<BlockState> redirectParse(Codec instance, DynamicOps dynamicOps, Object o, Operation<DataResult> original){
+    private static DataResult<BlockState> redirectParse(Codec<PalettedContainer<BlockState>> instance, DynamicOps<Tag> dynamicOps, Object o, Operation<DataResult<BlockState>> original){
 
         if(o instanceof CompoundTag tag){
 
-            tag.put("palette", tag.getList("palette", 10).stream().map((tag1) -> {
+            tag.put("palette", tag.getList("palette", 10).stream().peek((tag1) -> {
                 if(tag1 instanceof CompoundTag entry){
                     String name = entry.getString("Name");
                     if(name.contains("molten_vents")){
@@ -39,7 +39,6 @@ public class ChunkParserMixin {
                         entry.putString("Name", val);
                     }
                 }
-                return tag1;
             }).collect(new Collector<Tag, ListTag, Tag>() {
                 @Override
                 public Supplier<ListTag> supplier() {
